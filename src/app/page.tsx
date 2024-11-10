e0a8cc42c104811c33f02a3787702ee24cb7b60e
Ulises <ulisesapaza.280@gmail.com>
2024-11-10 17:27:45 -0300

    refactor: improve logic when is not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,38 @@ import { ButtonGoogleAuthorize } from "./components/ButtonGoogleAuthorize";
 
 export default async function Home() {
   const { isLoggedIn } = await auth();
-  let data;
-  if (isLoggedIn) {
-    data = await getEmails();
+
+  if (!isLoggedIn) {
+    return (
+      <div>
+        <h1 className="text-4xl">Autoriza con Google</h1>
+        <ButtonGoogleAuthorize />
+      </div>
+    )
   }
+
+  const data = await getEmails();
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      {
-        isLoggedIn ? (
-          <div>
-            <h1 className="text-4xl">Autorizado</h1>
-            <p>Ya puedes acceder a los documentos y correos de Google</p>
-            {data && data.messages ? <div>
+      <div>
+        <h1 className="text-4xl">Autorizado</h1>
+        <p>Ya puedes acceder a tus Google Docs y tus correos</p>
+        {data && data.messages ? <div>
 
-              <ul>
-                {data.messages.map((message) => (
-                  <li key={message.id}>
-                    <span>{message.snippet}</span>
-                    <Link href={`/email/${message.id}`}><a>Ver email</a></Link>
+          <ul className="flex flex-col gap-5">
+            {data.messages.map((message) => (
+              <li key={message.id}>
+                <span>{message.snippet ?? "No contiene texto"}</span>
+                <Link
+                className="bg-blue-400 text-white p-2 rounded"
+                
+                href={`/email/${message.id}`}>Ver email</Link>
 
-                  </li>
-                ))}
-              </ul>
-            </div> : null}
-          </div>
-        ) : (
-          <div>
-            <h1 className="text-4xl">Autoriza con Google</h1>
-            <ButtonGoogleAuthorize />
-          </div>
-        )
-      }
+              </li>
+            ))}
+          </ul>
+        </div> : null}
+      </div>
     </div>
   );
 }
