819d390e04893cc3706f9be447a5d6f6652a74d1
Ulises <ulisesapaza.280@gmail.com>
2024-11-10 09:23:13 -0300

    fix: add offline access to retrieve refresh token

--- src/app/actions/google.ts
+++ src/app/actions/google.ts
@@ -11,6 +11,8 @@ export async function getConsentScreenUri() {
     })
 
     return oauth.generateAuthUrl({
+        access_type: 'offline',
+        response_type: 'code',
         scope: [
             'https://www.googleapis.com/auth/documents',
         ]
