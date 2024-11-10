d299b039c5f370d0e703af0c6f2abdbc0bb61713
Ulises <ulisesapaza.280@gmail.com>
2024-11-10 17:15:43 -0300

    refactor: use new function and also render messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,41 @@
+import Link from "next/link";
 import { auth } from "./actions/auth";
+import { getEmails } from "./actions/google";
 import { ButtonGoogleAuthorize } from "./components/ButtonGoogleAuthorize";
 
-export  default async function Home() {
-  const {isLoggedIn} = await auth();
+export default async function Home() {
+  const { isLoggedIn } = await auth();
+  let data;
+  if (isLoggedIn) {
+    data = await getEmails();
+  }
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-  {
-    isLoggedIn ? (
-      <div>
-        <h1 className="text-4xl">Autorizado</h1>
-        <p>Ya puedes acceder a los documentos de Google</p>
-      </div>
-    ) : (
-      <div>
-        <h1 className="text-4xl">Autoriza con Google</h1>
-        <ButtonGoogleAuthorize />
-      </div>
-    )
-  }
+      {
+        isLoggedIn ? (
+          <div>
+            <h1 className="text-4xl">Autorizado</h1>
+            <p>Ya puedes acceder a los documentos y correos de Google</p>
+            {data && data.messages ? <div>
+
+              <ul>
+                {data.messages.map((message) => (
+                  <li key={message.id}>
+                    <span>{message.snippet}</span>
+                    <Link href={`/email/${message.id}`}><a>Ver email</a></Link>
+
+                  </li>
+                ))}
+              </ul>
+            </div> : null}
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-4xl">Autoriza con Google</h1>
+            <ButtonGoogleAuthorize />
+          </div>
+        )
+      }
     </div>
   );
 }
